eb250efe340aac3998372a407ed97f8de66b6657
Tollii <tolnes@protonmail.com>
2024-03-18 00:01:21 +0100

    Add tax rate

--- Client/Salary/Salary.cs
+++ Client/Salary/Salary.cs
@@ -2,43 +2,63 @@
 
 public class Salary(decimal yearly)
 {
+    private decimal _taxRatePercent = 0m;
+    
+    public decimal TaxRatePercent
+    {
+        get => _taxRatePercent;
+        set => _taxRatePercent = value > 100 ? 100 : value;
+    }
+
     public decimal Yearly { get; set; } = yearly;
+    public decimal YearlyAfterTax => decimal.Round(Yearly * (1 - TaxRatePercent / 100), 2);
 
     public decimal Monthly
     {
-        get => decimal.Round(Yearly / 12, 2);
+        get => decimal.Round(Yearly / 12, 2) * (1 - TaxRatePercent / 100);
         set => Yearly = value * 12;
     }
+    public decimal MonthlyAfterTax => decimal.Round(Monthly * (1 - TaxRatePercent / 100), 2);
 
     public decimal Weekly
     {
-        get => decimal.Round(Yearly / 52, 2);
+        get => decimal.Round(Yearly / 52, 2) * (1 - TaxRatePercent / 100);
         set => Yearly = value * 52;
     }
+    
+    public decimal WeeklyAfterTax => decimal.Round(Weekly * (1 - TaxRatePercent / 100), 2);
 
     public decimal Daily
     {
-        get => decimal.Round(Yearly / 260, 2);
+        get => decimal.Round(Yearly / 260, 2) * (1 - TaxRatePercent / 100);
         set => Yearly = value * 260;
     }
     
+    public decimal DailyAfterTax => decimal.Round(Daily * (1 - TaxRatePercent / 100), 2);
+    
     public decimal Hourly
     {
-        get => decimal.Round(Yearly / 1950, 2);
+        get => decimal.Round(Yearly / 1950, 2) * (1 - TaxRatePercent / 100);
         set => Yearly = value * 1950;
     }
     
+    public decimal HourlyAfterTax => decimal.Round(Hourly * (1 - TaxRatePercent / 100), 2);
+    
     public decimal ByMinute
     {
-        get => decimal.Round(Yearly / 1950 / 60, 2);
+        get => decimal.Round(Yearly / 1950 / 60, 2) * (1 - TaxRatePercent / 100);
         set => Yearly = value * 1950 * 60;
     }
     
+    public decimal ByMinuteAfterTax => decimal.Round(ByMinute * (1 - TaxRatePercent / 100), 2);
+    
     public decimal BySecond
     {
-        get => decimal.Round(Yearly / 1950 / 3600, 2);
+        get => decimal.Round(Yearly / 1950 / 3600, 2) * (1 - TaxRatePercent / 100);
         set => Yearly = value * 1950 * 3600;
     }
+    
+    public decimal BySecondAfterTax => decimal.Round(BySecond * (1 - TaxRatePercent / 100), 2);
     
     public override string ToString()
     {
@@ -51,6 +71,13 @@ public class Salary(decimal yearly)
             Hourly: {Hourly:0.##}
             By minute: {ByMinute:0.##}
             By second: {BySecond:0.##}
+            Yearly after tax: {YearlyAfterTax:0.##}
+            Monthly after tax: {MonthlyAfterTax:0.##}
+            Weekly after tax: {WeeklyAfterTax:0.##}
+            Daily after tax: {DailyAfterTax:0.##}
+            Hourly after tax: {HourlyAfterTax:0.##}
+            By minute after tax: {ByMinuteAfterTax:0.##}
+            By second after tax: {BySecondAfterTax:0.##}
             """;
     }
 }
